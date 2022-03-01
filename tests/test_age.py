14d1001932d3cac0c2bf4de6fea54352da2c7e07
Thanawat19 <teteeternal@gmail.com>
2022-03-01 20:51:08 +0700

    2

--- tests/test_age.py
+++ tests/test_age.py
@@ -11,3 +11,24 @@ def test_year_api():
     response = client.get("/service/getage?year="+input)
     assert response.status_code == 200
     assert response.json() == {"age": output}
+
+def test_year_zero_api():
+    input = "0"
+    output = "your input too less"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+def test_year_less_zero_api():
+    input = "-1"
+    output = "your input too less"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+def test_year_over_api():
+    input = "2566"
+    output = "your input too over"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
