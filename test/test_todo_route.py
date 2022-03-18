05fc52015adfdf0da0ae9714066e25fad9fef720
Thanawat19 <teteeternal@gmail.com>
2022-03-18 12:02:21 +0700

    updatedelete1

--- test/test_todo_route.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-import sys
-sys.path.insert(0, '../LAB_TEST06')
-from main import app
-
-client = TestClient(app)
-
-def test_post_todo_insert(db):
-    assert True
